10d3e11c6312a1c105e1696c9d2089b609f3f4b3
Clarence Ho <clarenceho@gmail.com>
2021-03-19 23:05:24 -0700

    Upgrade to OpenCV 4.5.1

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import cv2
+
 # Depends on your system, the OpenCV pre-trained classifiers for
 # face / eyes detection could be in different folder
 # FACE_CASCADE_FILE = "C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml"
-FACE_CASCADE_FILE = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
+FACE_CASCADE_FILE = cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml'
 
 # EYE_CASCADE_FILE = "C:/opencv/sources/data/haarcascades/haarcascade_eye.xml"
-EYE_CASCADE_FILE = "/usr/share/OpenCV/haarcascades/haarcascade_eye.xml"
+EYE_CASCADE_FILE = cv2.data.haarcascades + 'haarcascade_eye.xml'
 
 # Size of the face images used for training
 DEFAULT_FACE_SIZE = (200, 200)
